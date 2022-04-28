32b88a44859dd8442b2e9ae59575431156870c8c
CorneliusC7 <84184180+CorneliusC7@users.noreply.github.com>
2022-04-28 19:08:19 +0700

    e

--- firePost.js
+++ firePost.js
@@ -1,10 +1,6 @@
- // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
 import { getDatabase, ref, set, push, onChildAdded, query  } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
-
- // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDKdK4_z8MPN5q3jHCsnDg_PILW8McwdjM",
     authDomain: "thirtydaysboard.firebaseapp.com",
@@ -13,25 +9,14 @@ const firebaseConfig = {
     messagingSenderId: "346232856745",
     appId: "1:346232856745:web:449a260d1a2d498e0309b8"
 };
-
- // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const database = getDatabase(app);
 
 const dbRef = ref(database ,"board/")
-
-function writeUserData(subBoard, cont) {
+function writeBoard(cont) {
     set(ref(database, 'board/' + Date.now()), {
         content: cont
     });
 }
-function q(qry){
-    return document.querySelector(qry)
-}
-onChildAdded(query(dbRef), (snapshot) => {
-    console.log("child added");
-    console.log(snapshot.val()); // Logs newly added child
-    q('.sibotak').innerHTML += snapshot.val()
-});
-$('')
