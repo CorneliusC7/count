ba123dd7e67956f28adbc97453f7470101481086
CorneliusC7 <84184180+CorneliusC7@users.noreply.github.com>
2022-04-28 18:52:21 +0700

    e

--- fire.js
+++ fire.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
-import { getDatabase, ref, set, push, onChildAdded  } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+import { getDatabase, ref, set, push, onChildAdded, query  } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
